dba592720d393768fab221b49f3e5e75cbf80697
johnpl-io <john.nycjets@gmail.com>
2023-03-04 14:04:29 -0500

    more springboot changes

---
+++ src/main/java/com/coopercrew/crewconnect/Groupchat.java
@@ -0,0 +1,49 @@
+package com.coopercrew.crewconnect;
+import com.coopercrew.crewconnect.util.DataTransferObject;
+
+public class Groupchat implements DataTransferObject{
+    private Long gc_id;
+    private String group_name;
+    private int group_size;
+    private String date_created;
+
+    public long getId(){
+        return gc_id; //?!is this needed
+    }
+
+    public Long getGroupChatId(){
+        return gc_id;
+    }
+
+    public String getGroupName(){
+        return group_name;
+    }
+
+    public int getGroupSize(){
+        return group_size;
+    }
+
+    public String getDateCreated(){
+        return date_created;
+    }
+
+    public void setGroupChatId(Long gc_id){
+        this.gc_id = gc_id;
+    }
+
+    public void setGroupName(String group_name){
+        this.group_name = group_name;
+    }
+
+    public void setGroupSize(int group_size){
+        this.group_size = group_size;
+    }
+
+    public void setDateCreated(String date_created){
+        this.date_created = date_created;
+    }
+
+    public String toString(){
+        return "Groupchat{" + "groupchat id: " + gc_id + ", groupname: " + group_name + ", groupsize:" + group_size + ", date created: " + date_created + " }";
+    }
+}
