39cd7f003a2a32812485598644ee85ed1ba4a722
johnpl-io <john.nycjets@gmail.com>
2023-03-05 12:26:21 -0500

    another mistake actual pull req

--- src/main/java/com/coopercrew/crewconnect/web/UserController.java
+++ src/main/java/com/coopercrew/crewconnect/web/UserController.java
@@ -57,25 +57,7 @@ e.printStackTrace();
 }
 return user;
 }
-@GetMapping("/findByUsername/{username}") 
-public User findByEmail(@PathVariable String email){
-    DatabaseConnectionManager dcm = new DatabaseConnectionManager(hostname,
-    "crewconnect", "postgres", "password");
-    User user = new User();
-    
-    try {
-    Connection connection = dcm.getConnection();
-    UserDAO userDAO = new UserDAO(connection);
 
-    user = userDAO.findByEmail(email);
-
-    System.out.println(user);
-}
-catch(SQLException e) {
-e.printStackTrace();
-}
-return user;
-}
 
 
 }
