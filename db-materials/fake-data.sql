b8f87795c613430b614e4badecb04fbfc8d02660
colinh09 <colin.hwang@cooper.edu>
2023-03-04 22:00:40 -0500

    edited fake-data and create message script to match unix time

--- db-materials/fake-data.sql
+++ db-materials/fake-data.sql
@@ -28,15 +28,15 @@ VALUES
 (3, 4);
 
 -- Populate messages table
-INSERT INTO messages (gc_id, user_id, time_sent, message)
+INSERT INTO messages (gc_id, user_id, time_sent, message) 
 VALUES
-(1, 1, '2023-01-01', 'Hello GroupChat1!'),
-(1, 2, '2023-01-01', 'Hi User1!'),
-(1, 3, '2023-01-01', 'Greetings everyone!'),
-(1, 4, '2023-01-01', 'Hi everyone!'),
-(2, 2, '2022-12-31', 'Hello GroupChat2!'),
-(2, 3, '2022-12-31', 'Hi User2!'),
-(2, 1, '2022-12-31', 'Greetings everyone!'),
-(3, 1, '2023-01-02', 'Hello GroupChat3!'),
-(3, 3, '2023-01-02', 'Hi User1!'),
-(3, 4, '2023-01-02', 'Greetings everyone!');
+(1, 1, 1672531200, 'Hello GroupChat1!'),
+(1, 2, 1672531200, 'Hi User1!'),
+(1, 3, 1672531200, 'Greetings everyone!'),
+(1, 4, 1672531200, 'Hi everyone!'),
+(2, 2, 1640870400, 'Hello GroupChat2!'),
+(2, 3, 1640870400, 'Hi User2!'),
+(2, 1, 1640870400, 'Greetings everyone!'),
+(3, 1, 1672617600, 'Hello GroupChat3!'),
+(3, 3, 1672617600, 'Hi User1!'),
+(3, 4, 1672617600, 'Greetings everyone!');
