19093f3742e93c4b23cb0c2774aa843cdbfc0439
Jake Sigman <jsigman04@gmail.com>
2023-04-14 02:41:42 -0400

    UI Refresh

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import Login from './Login';
-import ChatList from './ChatList';
-import Chat from './Chat';
-import NewChat from './NewChat';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from 'firebase/auth';
 import firebase from 'firebase/app';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import Fab from '@mui/material/Fab';
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+ import Link from '@mui/material/Link';
+
+
 // Main application
 
 const App = () => {
@@ -17,7 +33,12 @@ const App = () => {
     const [inputPassword, setInputPassword] = useState("");
     const [id, setId] = useState(0); // Add this line to manage `id` using useState
     const [inputEmail, setInputEmail] = useState("");
+    const [message, setMessage] = useState('');
+    const [users, setUsers] = useState([]);
+    const [chatName, setChatName] = useState("My Chat ");
+    const [isOpen, setIsOpen] = useState(false);
 
+    const theme = createTheme();
     // use effect hook for login
     // useEffect(() => {
     //     if (!inputUsername || !inputPassword) return;
@@ -237,38 +258,160 @@ const App = () => {
     } catch (error) {
         console.error("Error sending message:", error);
         }   
+        
     };
       
+    const handleDeleteMessage = () => {
+        console.log("Message Delete!");
+    }
+
+    const handleLeave = () => {
+        console.log("Bye!");
+    }
 
     // Showing the selected chat
     if (selectedChatId) {
         const selectedChat = chats.find((chat) => chat.id === selectedChatId);
-        return ( <Chat 
-            chat={selectedChat} 
-            setSelectedChatId={setSelectedChatId} 
-            onSendMessage={handleSendMessage}
-            />
-        );
+        // return ( <Chat 
+        //     chat={selectedChat} 
+        //     setSelectedChatId={setSelectedChatId} 
+        //     onSendMessage={handleSendMessage}
+        //     />
+        // );
     }
+    
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            handleSendMessage(message);
+        }
+    };
+
+    const handleNewMessageChange = (event) => {
+
+        if (event.target.value === "") {
+            return;
+        }
+
+        setMessage(event.target.value);
+    };
+
+    // Function for toggling the popup
+
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
+
+    // Handler for when the form to create the chat is submitted
+
+    const handlePopupCreate = (event) => {
+        event.preventDefault();
+        handleCreateChat(users, chatName);
+        togglePopup();
+    };
+
 
     // Return main HTML for home page
     return (
-        <div className='page'>
-            <h1>CrewConnect</h1>
-            <NewChat
-                show={selectedChatId != null}
-                onCreate={handleCreateChat}
-            />
-            <ChatList
-                chats={chats}
-                onSelect={chatId => handleSelectChat(chatId)}
-            />
-            <button onClick={() => { 
-                signOut(getAuth()); 
-                handleLogout(); 
-                }} 
-                className="bottom" 
-                >Log Out</button>
+        <div>
+            <ThemeProvider theme={theme}> 
+                <Grid container component={Paper}>
+                    <Grid item xs={12} >
+                        <Typography variant="h5" className="header-message">CrewConnect</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container component={Paper}>
+                    <Grid item xs={3}>
+                        <Button onClick={() => { 
+                            signOut(getAuth()); 
+                            handleLogout(); 
+                            }} 
+                            className="bottom" 
+                            >Log Out</Button>
+                        <Button onClick={togglePopup}>New Chat</Button>
+                        {isOpen && (<>
+                                <h3>Create New Chat</h3>
+                                <Grid>
+                                    <TextField
+                                        type="text"
+                                        id="name"
+                                        placeholder="Chat Name"
+                                        size="15"
+                                        onChange={(event) => {setChatName(event.target.value);}}
+                                    />
+                                    <TextField
+                                        type="text" 
+                                        id="users" 
+                                        placeholder="Users"
+                                        size="85"
+                                        required
+                                        value={users} 
+                                        onChange={(event) => setUsers(event.target.value.split(","))} 
+                                    />
+                                    <Button onClick={handlePopupCreate}>Create</Button> <Button onClick={togglePopup}>CANCEL</Button>
+                                </Grid></>
+                        )}
+                
+                    <List>
+                        <ListItem button key={id}>
+                            <ListItemIcon>
+                                <Avatar src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.personality-insights.com%2Fdefault-profile-pic%2F&psig=AOvVaw1CZ4o8DO5MP2OWNPx80G7p&ust=1681537567328000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOCSy-DVqP4CFQAAAAAdAAAAABAR" />
+                            </ListItemIcon>
+                            <ListItemText primary={id}></ListItemText>
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <Divider />
+                    <List>
+                    <h3>Your Chats</h3>
+                        {chats.map((chat) => (
+                            <ListItem button key={chat.id} onClick={() => handleSelectChat(chat.id)}>
+                            <ListItemIcon>
+                                <Avatar alt={chat.name} src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.personality-insights.com%2Fdefault-profile-pic%2F&psig=AOvVaw1CZ4o8DO5MP2OWNPx80G7p&ust=1681537567328000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOCSy-DVqP4CFQAAAAAdAAAAABAR" />
+                            </ListItemIcon>
+                            <ListItemText primary={chat.name}>{chat.name}</ListItemText>
+                            <ListItemText secondary={chat.users.map((user) => (
+                                <span>{user}&emsp;</span>
+                                ))} align="right"></ListItemText>
+                            <Link href="#" variant="body2" onClick={handleLeave}>
+                                {"Leave"}
+                            </Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+                <Grid item xs={9}>
+                    <h3>{chats.find((chat) => chat.id === selectedChatId).name}</h3>
+                    <List>
+                        {selectedChatId && (
+                            <Grid container>
+                                {chats.find((chat) => chat.id === selectedChatId).messages.map((message, index) => (
+                                    <Grid item xs={12}>
+                                        <ListItemText align="left" primary={message.text} secondary={message.sender}></ListItemText>
+                                        <Link href="#" variant="body2" onClick={handleDeleteMessage}>
+                                            {"Delete"}
+                                        </Link>
+                                    </Grid>
+                                ))} 
+                                
+                                <Grid item xs={11}>
+                                    <TextField id="outlined-basic-email" label="Type Something" 
+                                        value={message} 
+                                        onChange={handleNewMessageChange} 
+                                        onKeyPress={handleKeyPress} 
+                                    fullWidth />
+                                </Grid>
+                                <Grid xs={1} align="right">
+                                    <Fab color="primary" aria-label="add" onClick={handleSendMessage}><SendIcon /></Fab>
+                                </Grid>
+                            </Grid>
+                        )}
+                    </List>
+                    <Divider/>
+                </Grid>
+                </Grid>
+            </ThemeProvider>
         </div>
     );
   
